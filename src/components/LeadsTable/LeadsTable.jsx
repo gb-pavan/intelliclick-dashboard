ed492603b9f2b3fe33bef2cf33c559e901c49903
pavan <pavankmr1902@gmail.com>
2024-11-16 15:31:13 +0530

    changed eye icon background and added bottom to the table for pagination

--- src/components/LeadsTable/LeadsTable.jsx
+++ src/components/LeadsTable/LeadsTable.jsx
@@ -32,8 +32,80 @@ const leadsData = [
     createdBy: "Online",
     createdAt: "4 APR, 2023 4:35PM",
   },
-  // Add more data as per the UI.
+  {
+    studentName: "Priya Sharma",
+    class: "7th",
+    phoneNumber: "+91 876757XXXX",
+    status: "Prospect",
+    submittedBy: "Father",
+    createdBy: "Offline",
+    createdAt: "5 APR, 2023 10:10AM",
+  },
+  {
+    studentName: "Aman Verma",
+    class: "10th",
+    phoneNumber: "+91 876758XXXX",
+    status: "Qualified",
+    submittedBy: "Mother",
+    createdBy: "Online",
+    createdAt: "6 APR, 2023 2:15PM",
+  },
+  {
+    studentName: "Sanya Yadav",
+    class: "8th",
+    phoneNumber: "+91 876759XXXX",
+    status: "Prospect",
+    submittedBy: "Father",
+    createdBy: "Offline",
+    createdAt: "7 APR, 2023 5:00PM",
+  },
+  {
+    studentName: "Vikram Singh",
+    class: "12th",
+    phoneNumber: "+91 876760XXXX",
+    status: "Qualified",
+    submittedBy: "Mother",
+    createdBy: "Online",
+    createdAt: "8 APR, 2023 9:45AM",
+  },
+  {
+    studentName: "Ria Patel",
+    class: "6th",
+    phoneNumber: "+91 876761XXXX",
+    status: "Prospect",
+    submittedBy: "Father",
+    createdBy: "Offline",
+    createdAt: "9 APR, 2023 12:00PM",
+  },
+  {
+    studentName: "Ishaan Kapoor",
+    class: "11th",
+    phoneNumber: "+91 876762XXXX",
+    status: "Qualified",
+    submittedBy: "Mother",
+    createdBy: "Online",
+    createdAt: "10 APR, 2023 1:20PM",
+  },
+  {
+    studentName: "Neha Agarwal",
+    class: "10th",
+    phoneNumber: "+91 876763XXXX",
+    status: "Prospect",
+    submittedBy: "Father",
+    createdBy: "Offline",
+    createdAt: "11 APR, 2023 3:40PM",
+  },
+  {
+    studentName: "Tarun Mehta",
+    class: "8th",
+    phoneNumber: "+91 876764XXXX",
+    status: "Qualified",
+    submittedBy: "Mother",
+    createdBy: "Online",
+    createdAt: "12 APR, 2023 11:30AM",
+  },
 ];
+
 
 function LeadsTable() {
   return (
@@ -91,13 +163,21 @@ function LeadsTable() {
                 <td>{lead.submittedBy}</td>
                 <td>{lead.createdBy}</td>
                 <td>
-                  <button><IoEyeOutline size={25} /></button>
+                  <button className="eye-button"><IoEyeOutline size={25} /></button>
                 </td>
-                <td>{lead.createdAt}</td>
+                <td className="last">{lead.createdAt}</td>
               </tr>
             ))}
           </tbody>
         </table>
+        <div className="table-bottom">
+          <div>
+            hello
+          </div>
+          <div>
+            bolo
+          </div>
+        </div>
     </div>
     </>
    
